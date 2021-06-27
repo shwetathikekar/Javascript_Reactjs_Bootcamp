447ae12aa4c14ad6ce3fed984fdbe04dc2623488
shwetathikekar <72989187+shwetathikekar@users.noreply.github.com>
2021-06-27 18:51:11 +0530

    Add files via upload

---
+++ Info1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and Backend with NodeJS - 7 Day Bootcamp</h1>
+      <p>
+        Here we bring our another free BOOTCAMP with a hands-on project at the
+        end Foundation of Javascript
+      </p>
+    </div>
+  );
+}
+
+export default Info;
